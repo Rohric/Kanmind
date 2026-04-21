fc20d61f6018b3c3218d37682839d125f0312ce2
Emil Marsal <emilmarsal@icloud.com>
2026-04-21 10:28:51 +0200

    add admin panel

--- backend/board_app/admin.py
+++ backend/board_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from . models import Board, BoardMembership
+from task_app.models import Task
+# Register your models here
 
-# Register your models here.
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 0  # Verhindert, dass standardmäßig leere Formulare angezeigt werden
+    fields = ('title', 'status', 'priority', 'assignee', 'due_date')
+    readonly_fields = fields
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner")
+    list_filter = ("owner",)
+    search_fields = ("title",)
+    inlines = [TaskInline]
+
+
+@admin.register(BoardMembership)
+class BoardMembershipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'board', 'role', 'joined_at')
+    list_filter = ('user', 'board')
+    search_fields = ('user__username', 'board__title')
--- backend/task_app/admin.py
+++ backend/task_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Task, Comment
 
 # Register your models here.
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0  # Verhindert, dass standardmäßig leere Formulare angezeigt werden
+    readonly_fields = ('user', 'created_at')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "board", "status",
+                    "due_date", "priority", "assignee", "reviewer", "creator",)
+    search_fields = ("title",)
+    list_filter = ("status", "priority", "board")
+    inlines = [CommentInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'task', 'user', 'created_at')
+    list_filter = ('user', 'task')
+    search_fields = ('content',)
