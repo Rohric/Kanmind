abe5ded8e804832d04e6566317de6fe9c3c9a4d2
Emil Marsal <emilmarsal@icloud.com>
2026-07-14 22:21:18 +0200

    feat: register DemoResetMiddleware

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "user_auth_app.middleware.DemoResetMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
